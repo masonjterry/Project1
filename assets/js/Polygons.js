fc04ca743d2069fa1652d030e5487410573316b2
Carlos <gilbertogutierrez@CarlosMacbookPro.local>
2017-11-28 13:27:15 -0600

    Polylines and geolocation are good but need Polygons instead

---
+++ assets/js/Polygons.js
@@ -0,0 +1,71 @@
+var poly;
+var map;
+var infoWindow
+
+//find user location
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 30.2870379, lng: -97.7313409},
+        zoom: 17
+	});
+	infoWindow = new google.maps.InfoWindow;
+
+	//GEOLOCATION 
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+        	var pos = {
+            	lat: position.coords.latitude,
+            	lng: position.coords.longitude
+        	};
+
+        	infoWindow.setPosition(pos);
+        	infoWindow.setContent('Location found.');
+        	infoWindow.open(map);
+        	map.setCenter(pos);
+    	}, 
+
+    	function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+    //MAP PINS AND POLYLINES
+	poly = new google.maps.Polyline({
+          strokeColor: '#000000',
+          strokeOpacity: 1.0,
+          strokeWeight: 3
+    });
+    poly.setMap(map);
+
+    // Add a listener for the click event
+	map.addListener('click', addLatLng);
+};
+
+
+//DROP PIN AND DRAW LINE ON CLICK
+function addLatLng(event) {
+    var path = poly.getPath();
+
+    // Because path is an MVCArray, we can simply append a new coordinate
+    // and it will automatically appear.
+    path.push(event.latLng);
+
+    // Add a new marker at the new plotted point on the polyline.
+    var marker = new google.maps.Marker({
+    	position: event.latLng,
+        title: '#' + path.getLength(),
+        map: map
+    });
+};
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+};
